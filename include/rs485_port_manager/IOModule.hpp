5c2015ce7d57accab4fdf4558ea4a95c0428c485
Raees <sonia2@sonia-dory.com>
2025-06-29 22:30:00 -0400

    Added the IOModule File

---
+++ include/rs485_port_manager/IOModule.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <stdio.h>
+
+#include <functional>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_srvs/srv/empty.hpp>
+#include <thread>
+#include <tuple>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+#include "sonia_common_cpp/SerialConn.hpp"
+#include "sonia_common_cpp/SharedQueue.hpp"
+#include "sonia_common_ros2/msg/kill_status.hpp"
+#include "sonia_common_ros2/msg/mission_status.hpp"
+#include "InterfaceModule.hpp"
+#include "sonia_common_ros2/msg/mission_status.hpp"
+namespace module
+{
+
+    class KillProvider : public rclcpp::Node, InterfaceModule
+    {
+        
+        public:
+            KillProvider();
+            ~KillProvider();
+
+        /**
+         * @brief Kill all internal threads.
+         */
+        void Kill();
+
+        /**
+         * @brief
+         *
+         * @param status
+         */
+        void sendMessage(queueObject queue);
+
+        /**
+         * @brief
+         *
+         * @param msg
+         */
+        void messageRS485CallBack(const sonia_common_ros2::msg::RS485msg &msg);
+
+        private:
+        /**
+         * @brief Polls the status of kill and mission via RS485 connection.
+         *
+         * This function sends requests to retrieve the status of kill and mission
+         * via the RS485 connection.
+         */
+        void pollKillMission();
+
+        /**
+         * @brief
+         *
+         * @param status
+         */
+        void publishKill(bool status);
+
+        /**
+         * @brief
+         *
+         * @param status
+         */
+        void publishMission(bool status);
+
+        /**
+         * @brief
+         *
+         * @param status
+         */
+
+        /* Used to publish the information of the Kill Switch */
+        rclcpp::Publisher<sonia_common_ros2::msg::KillStatus>::SharedPtr _publisherKill;
+        rclcpp::Publisher<sonia_common_ros2::msg::RS485msg>::SharedPtr _publishers485;
+        rclcpp::Publisher<sonia_common_ros2::msg::MissionStatus>::SharedPtr _publisherMission;
+        rclcpp::TimerBase::SharedPtr _timerKillMission;
+    };
+
+}  // namespace KillManager
---
+++ src/IOModule.cpp
@@ -0,0 +1,87 @@
+#include "rs485_port_manager/IOModule.hpp"
+
+using namespace std::chrono_literals;
+
+namespace module
+{
+    KillProvider::KillProvider() // Node constructor
+    : Node("rs485_kill_switch")
+    {
+        _publisherKill = this->create_publisher<sonia_common_ros2::msg::KillStatus>("/kill_switch_rs485/kill_status", 10);
+        _publishers485 = this->create_publisher<sonia_common_ros2::msg::RS485msg>("/rs485/msgToSend", 10);
+        _publisherMission =
+            this->create_publisher<sonia_common_ros2::msg::MissionStatus>("/provider_rs485/mission_status", 10);
+        _timerKillMission = this->create_wall_timer(500ms, std::bind(&KillProvider::pollKillMission, this));
+    }
+    KillProvider::~KillProvider() {}
+
+    void KillProvider::sendMessage(queueObject queue)
+    {
+        auto to_return = sonia_common_ros2::msg::RS485msg();
+        to_return.slave = queue.slave;
+        to_return.cmd = queue.cmd;
+        to_return.data = queue.data;
+
+        _publishers485->publish(to_return);
+    }
+    void KillProvider::pollKillMission()
+    {
+        queueObject msg;
+        msg.data.push_back(0x00);
+
+        // Transmit request to get kill status
+        msg.slave = _SlaveId::SLAVE_KILLMISSION;
+        msg.cmd = _Cmd::CMD_KILL;
+        
+        // sendMessage(msg);
+
+        // Wait for a short duration to allow for processing... Embeded restriction
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        
+        // Transmit request to get mission status
+        msg.slave = _SlaveId::SLAVE_KILLMISSION;
+        msg.cmd = _Cmd::CMD_MISSION;
+
+        // sendMessage(msg);
+    }
+    void KillProvider::messageRS485CallBack(const sonia_common_ros2::msg::RS485msg &msg)
+    {
+        queueObject ser;
+        ser.cmd = msg.cmd;
+        ser.slave = msg.slave;
+        ser.data.clear();
+
+        std::vector<uint8_t> uint8Vector;
+        uint8Vector.resize(msg.data.size());
+
+        std::transform(msg.data.begin(), msg.data.end(), uint8Vector.begin(),
+                   [](char c) { return static_cast<uint8_t>(c); });
+        
+        switch (ser.cmd)
+        {
+            case _Cmd::CMD_KILL:
+                // get data value
+                // publish on kill publisher
+                publishKill(ser.data[0] == 1);
+                break;
+            case _Cmd::CMD_MISSION:
+                // get data value
+                // publish on mission publisher
+                publishMission(ser.data[0] == 1);
+                break;
+        }
+
+    }
+    void KillProvider::publishKill(bool status)
+    {
+        sonia_common_ros2::msg::KillStatus state;
+        state.status = status;
+        _publisherKill->publish(state);
+    }
+    void KillProvider::publishMission(bool status)
+    {
+        sonia_common_ros2::msg::MissionStatus state;
+        state.status = status;
+        _publisherMission->publish(state);
+    }
+}
