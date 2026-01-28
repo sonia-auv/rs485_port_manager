3a9fdb132e5b30cd799e62de7d3beaa4fb6d4ff8
LogSonia <log.club.sonia@etsmtl.net>
2026-01-28 12:30:11 -0500

    LOG-151: fixiing publisher

--- src/RS485Provider.cpp
+++ src/RS485Provider.cpp
@@ -14,10 +14,10 @@ RS485Provider* RS485Provider::_instance= nullptr;;
     {
         ObservateurInterfaceModule = {};
 
-        _publisherNodeStatus = this->create_publisher<sonia_common_ros2::msg::NodeStatus>("/system_monitor/node_status", 1);
-        _timerNodeStatus = this->create_wall_timer(500ms, std::bind(&RS485Provider::publishStatus, this));
+        _publisherNodeStatus = _instance->create_publisher<sonia_common_ros2::msg::NodeStatus>("/system_monitor/node_status", 1);
+        _timerNodeStatus = _instance->create_wall_timer(500ms, std::bind(&RS485Provider::publishStatus, _instance));
 
-        _node_status.node_name = this->get_name();
+        _node_status.node_name = _instance->get_name();
         _node_status.quality = sonia_common_ros2::msg::NodeStatus::Q_OK;
         _node_status.state = sonia_common_ros2::msg::NodeStatus::STATE_INITIALIZING;
 
@@ -85,8 +85,8 @@ RS485Provider* RS485Provider::_instance= nullptr;;
     }
 
     void RS485Provider::publishStatus(){
-        RCLCPP_INFO(this->get_logger(), "publishing node status: %s", _node_status.node_name.c_str());
-        _node_status.stamp = this->now();
+        RCLCPP_INFO(_instance->get_logger(), "publishing node status: %s", _node_status.node_name.c_str());
+        _node_status.stamp = _instance->now();
         _publisherNodeStatus->publish(_node_status);
     }
 
